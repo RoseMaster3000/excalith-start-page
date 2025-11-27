2989c3b84cb9ff3aeeb0cf5f9651d863ee40f3d6
Shahrose Kasim <rosemaster3000@gmail.com>
2025-11-27 00:24:46 -0500

    linted

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -1,28 +1,28 @@
 // src/pages/api/login.js
-import { serialize } from 'cookie'
-import { compare } from 'bcryptjs'
+import { serialize } from "cookie"
+import { compare } from "bcryptjs"
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST') return res.status(405).end()
-  const { password } = req.body
-  const HASHED_PASSWORD = "$2b$10$9hLas6F44oYVLkyNiyz4k.bXyo2pwU5kLmvytffjYKyrBpq8vwUZe"
+	if (req.method !== "POST") return res.status(405).end()
+	const { password } = req.body
+	const HASHED_PASSWORD = "$2b$10$9hLas6F44oYVLkyNiyz4k.bXyo2pwU5kLmvytffjYKyrBpq8vwUZe"
 
-  // Compare the input password with the stored hash
-  const match = await compare(password, HASHED_PASSWORD)
+	// Compare the input password with the stored hash
+	const match = await compare(password, HASHED_PASSWORD)
 
-  if (match) {
-    // Set a cookie that lasts for 30 days
-    const cookie = serialize('my_secret_auth', 'true', {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
-      maxAge: 60 * 60 * 24 * 30, // 30 days
-      path: '/',
-    })
+	if (match) {
+		// Set a cookie that lasts for 30 days
+		const cookie = serialize("my_secret_auth", "true", {
+			httpOnly: true,
+			secure: process.env.NODE_ENV === "production",
+			sameSite: "strict",
+			maxAge: 60 * 60 * 24 * 30, // 30 days
+			path: "/"
+		})
 
-    res.setHeader('Set-Cookie', cookie)
-    return res.status(200).json({ success: true })
-  }
+		res.setHeader("Set-Cookie", cookie)
+		return res.status(200).json({ success: true })
+	}
 
-  return res.status(401).json({ error: 'Invalid password' })
+	return res.status(401).json({ error: "Invalid password" })
 }
